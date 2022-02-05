e6d6151e293159b14c6cfbbe687d51b4ef3feb4c
Akhilesh <aakakhilesh@gmail.com>
2022-02-06 02:11:22 +0530

    c6

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,9 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt','This file was created by Node.js!');
+const getNotes = require('./notes.js');
+
+const msg = getNotes();
+
+console.log(msg);
+
+// const add = require('./utils.js');
+// const sum = add(4,-2);
+// console.log(`${sum}`);

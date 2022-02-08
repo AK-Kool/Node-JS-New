5bae486ab737f5ba22fde6fa1ff1f598462ef15a
Akhilesh <aakakhilesh@gmail.com>
2022-02-09 02:36:42 +0530

    process obj commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,7 @@
-import chalk from 'chalk';
-// const validator = require('validator');
-// const getNotes = require('./notes.js');
-
-// const msg = getNotes();
-
-// console.log(msg);
-
-// console.log(validator.isEmail('@xyz.com'));
-
-console.log(chalk.green('Success!!'));
-
-// const add = require('./utils.js');
-// const sum = add(4,-2);
-// console.log(`${sum}`);
+const command = process.argv[2];
+const log = console.log;
+if(command === 'add'){
+    log(`Adding Note..`);
+} else if(command === 'remove'){
+    log(`Removing Note...`);
+}

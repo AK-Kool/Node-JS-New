a2235f221f02762930e8f7c3b0347154be988610
Akhilesh <aakakhilesh@gmail.com>
2022-02-09 02:05:55 +0530

    chalk commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,14 @@
-const getNotes = require('./notes.js');
+import chalk from 'chalk';
+// const validator = require('validator');
+// const getNotes = require('./notes.js');
 
-const msg = getNotes();
+// const msg = getNotes();
 
-console.log(msg);
+// console.log(msg);
+
+// console.log(validator.isEmail('@xyz.com'));
+
+console.log(chalk.green('Success!!'));
 
 // const add = require('./utils.js');
 // const sum = add(4,-2);
